878275f3d2c676ae0d3ecad274727abda223bf31
Ivaylo Yordanov <ivayloyordanov222@gmail.com>
2024-09-14 20:07:57 +0300

    Initial commit

---
+++ src/main/kotlin/com/ivo/book_network/BookNetworkApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ivo.book_network
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookNetworkApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookNetworkApiApplication>(*args)
+}
